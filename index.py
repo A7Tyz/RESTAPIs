c4d5322ab272c2a6e1c833d6c8aa8a93eaac3bdb
A7Tyz <121413084+A7Tyz@users.noreply.github.com>
2023-03-22 15:48:40 +0700

    Add files via upload

--- index.py
+++ index.py
@@ -2,55 +2,58 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-countries = [
-    {"id" : '1' , "name" : "Thailand" , "capital" : "Bangkok" },
-    {"id" : '2' , "name" : "Australia" , "capital" : "Canberra" },
-    {"id" : '3' , "name" : "USA" , "capital" : "LA" },
+Items = [
+    {"name": "product name", "category:" : "1" , "price:" : "20.5" , "instock:" : "200"},
+    
 ]
-def _find_next_id(id):
-    data = [x for x in countries if  x['id']==id]
+def _find_next_id(name):
+    data = [x for x in Items if  x['name']==name]
     return data
 
 @app.route('/country', methods=["GET"])
 def get_country():
-    return jsonify(countries)
+    return jsonify(Items)
 
-@app.route('/country/<id>', methods=["GET"])
-def get_country_id(id):
-    data = _find_next_id(id)
+@app.route('/country/<name>', methods=["GET"])
+def get_country_id(name):
+    data = _find_next_id(name)
     return jsonify(data)
 
-@app.route('/country' , methods=["POST"])
+
+@app.route('/' , methods=["POST"])
 def post_country():
-    id = request.form.get('id')
     name = request.form.get('name')
-    capital = request.form.get('capital')
+    category = request.form.get('category')
+    price = request.form.get('price')
+    instock = request.form.get('instock')
+    
 
     new_data = {
-        "id" : id,
         "name" : name,
-        "capital" : capital
+        "category" : category,
+        "price" : price,
+        "instock" : instock
     }
 
-    if (_find_next_id(id)) :
-        return {"error" : "Bad Request"}, id
+    if (_find_next_id(name)) :
+        return {"error" : "Bad Request"}, name
     else : 
-        countries.append(new_data)
-        return jsonify(countries)
+        Items.append(new_data)
+        return jsonify(Items)
 
-@app.route('/country/<id>', methods = ['DELETE'])
-def delete_country(id):
+@app.route('/country/<name>', methods = ['DELETE'])
+def delete_country(name):
 
-    data = _find_next_id(id)
+    data = _find_next_id(name)
     if not data:
-        return {"error": "Country not found"}, 404
+        return {"error": "country not found"}, 404
     else:
-        countries.remove(data[0]) 
-        return "Country deleted succuessfully" ,200
-
-@app.route('/country/<id>', methods=['PUT'])
-def put_country(id):
-    global countries
+        Items.remove(data[0]) 
+        return "country deleted succuessfully" ,200
+""""
+@app.route('/Itemm/<id>', methods=['PUT'])
+def put_Itemm(id):
+    global Items
     name = request.form.get('name')
     capital = request.form.get('capital')
 
@@ -59,23 +62,22 @@ def put_country(id):
         "capital": capital
     }
 
-    for country in countries:
-        if id == country.get("id"):
-            country["name"] = str(name)
-            country['capital'] = str(capital)
-            return jsonify(countries)
+    for Itemm in Items:
+        if id == Itemm.get("id"):
+            Itemm["name"] = str(name)
+            Itemm['capital'] = str(capital)
+            return jsonify(Items)
         else:
             return "error", 404
-@app.route('/country/<id>', methods=["PATCH"])
-def patch_country(id: int):
-    country = _find_next_id(id)
-    if country is None:
-        return jsonify({'error':'Country not found!'}),404
+@app.route('/Itemm/<id>', methods=["PATCH"])
+def patch_Itemm(id: int):
+    Itemm = _find_next_id(id)
+    if Itemm is None:
+        return jsonify({'error':'Itemm not found!'}),404
 
-    updated_country = jsonify.loads(request.data)
-    country.update(updated_country)
-    return jsonify(countries)
-
-
+    updated_Itemm = jsonify.loads(request.data)
+    Itemm.update(updated_Itemm)
+    return jsonify(Items)
+"""
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
